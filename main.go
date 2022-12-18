f7fe8c586c9e5dd46f1f7e5399da7fa7e688fda1
PUMAmg <puma.mw2@gmail.com>
2022-12-18 07:10:15 +0300

    New lesson

--- main.go
+++ main.go
@@ -3,9 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var ver = "v0.0.1"
-	var id int
-	pi := 3.1415
-
-	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
+	for i := 1; i <= 100; i++ {
+		/* if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}*/
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
 }
