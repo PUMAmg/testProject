90bb46055c3e550972c4e9dae9952b7c7674b438
PUMAmg <puma.mw2@gmail.com>
2022-12-18 05:18:10 +0300

    New lesson

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var ver = "v0.0.1"
-	var id int
-	pi := 3.1415
-
-	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
+	var dateOfBirth int
+	switch {
+	case dateOfBirth >= 1946 && dateOfBirth < 1965:
+		fmt.Println("Привет, бумер!")
+	case dateOfBirth >= 1965 && dateOfBirth < 1981:
+		fmt.Println("Привет, представитель X!")
+	case dateOfBirth >= 1981 && dateOfBirth < 1997:
+		fmt.Println("Привет, миллениал!")
+	case dateOfBirth >= 1997 && dateOfBirth < 2013:
+		fmt.Println("Привет, зумер!")
+	case dateOfBirth >= 2013:
+		fmt.Println("Привет, альфа!")
+	default:
+		fmt.Println("Приветствую, о великий старец!")
+	}
 }
