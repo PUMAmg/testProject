5e8e7463e12d16cd1519eb5d945901ad0be0b21a
PUMAmg <puma.mw2@gmail.com>
2022-12-20 03:14:47 +0300

    New lesson

--- main.go
+++ main.go
@@ -3,9 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	var ver = "v0.0.1"
-	var id int
-	pi := 3.1415
+	weekTempArr := [7]int{1, 2, 3, 4, 5, 6, 7}
+	workDaysSlice := weekTempArr[:5]
+	weekendSlice := weekTempArr[5:]
+	fromTuesdayToThursDaySlice := weekTempArr[1:4]
+	weekTempSlice := weekTempArr[:]
 
-	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
+	// fromTuesdayToThursDaySlice[2] = 0   изменение элементов слайса
+
+	fmt.Println(workDaysSlice, len(workDaysSlice), cap(workDaysSlice))
+	fmt.Println(weekendSlice, len(weekendSlice), cap(weekendSlice))
+	fmt.Println(fromTuesdayToThursDaySlice, len(fromTuesdayToThursDaySlice), cap(fromTuesdayToThursDaySlice))
+	fmt.Println(weekTempSlice, len(weekTempSlice), cap(weekTempSlice))
+	fmt.Println(weekTempArr)
+
+	/* Уменьшение размера слайсла
+	weekTempSlice = weekTempSlice[:len(weekTempSlice)-1]
+	fmt.Println(weekTempSlice, len(weekTempSlice), cap(weekTempSlice))
+	*/
+
+	/*Увеличение размера слайса с созданием нового базового массива
+	newSlice := append(weekTempSlice, 8)
+	fmt.Println(weekTempSlice, len(weekTempSlice), cap(weekTempSlice))
+	fmt.Println(newSlice, len(newSlice), cap(newSlice))
+	*/
+
+	/*Увеличение размера слайса с тем же базовы массивом
+	newSlice := append(workDaysSlice, 8)
+	fmt.Println(newSlice, len(newSlice), cap(newSlice))
+	newSlice[2] = 0
+	fmt.Println(newSlice, len(newSlice), cap(newSlice))
+	fmt.Println(weekTempArr)
+	*/
+
 }
