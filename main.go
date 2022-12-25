87949de1e38bf83467d024a11796207a1b897504
PUMAmg <puma.mw2@gmail.com>
2022-12-25 08:57:39 +0300

    New lesson

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var ver = "v0.0.1"
-	var id int
-	pi := 3.1415
-
-	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
+	prices := map[string]int{"хлеб": 50, "молоко": 100, "масло": 200, "колбаса": 500, "соль": 20, "огурцы": 200, "сыр": 600, "ветчина": 700, "буженина": 900, "помидоры": 250, "рыба": 300, "хамон": 1500}
+	order := []string{"хлеб", "буженина", "сыр", "огурцы"}
+	fmt.Println("Список деликатесов:")
+	for k, v := range prices {
+		if v >= 500 {
+			fmt.Println(k)
+		}
+	}
+	orderSum := 0
+	for _, v := range order {
+		for k, vm := range prices {
+			if v == k {
+				orderSum += vm
+			}
+		}
+	}
+	fmt.Printf("Стоимость заказа - %d", orderSum)
 }
